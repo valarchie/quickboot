753b294c8d079c0e2b5b87f7a0365405c9f8bc1f
valarchie <343928303@qq.com>
2020-08-10 21:59:02 +0800

    feat: eventbus
    新增eventbus支持

---
+++ src/main/java/com/valarchie/quickboot/application/common/event/MessageListener.java
@@ -0,0 +1,19 @@
+package com.valarchie.quickboot.application.common.event;
+
+import com.google.common.eventbus.Subscribe;
+
+/**
+ * description: 消息事件
+ *
+ * @author: valarchie
+ * on: 2020/8/10
+ * @email: 343928303@qq.com
+ */
+public class MessageListener {
+
+    @Subscribe
+    public void listen(EBMessage message) {
+        System.out.println("Event bus receive message:" + message.getMessage());
+    }
+
+}
